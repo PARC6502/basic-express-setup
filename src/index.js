5f68e65716a382e8596db4ab9615dcbb361ab947
parc6502 <ibrahim@serafy.co.uk>
2019-03-18 12:13:39 +0000

    basic express setup

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
-require('dotenv/config')
+require("dotenv/config");
+const express = require("express");
+const app = express();
+const cors = require("cors")
 
-console.log("Woo! I'm a node app!")
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => console.log(`app listening on port ${PORT}`));
+
+/* example middle ware that logs request information */
+app.use((req, _res, next) => {
+  let requestLog = `${req.ip} requested path '${req.path}' at ${new Date(
+    Date.now()
+  )}`;
+  console.log(requestLog);
+  next();
+});
+
+/* By default all routes are accessible for all domains now */
+app.use(cors())
+
+/* ROUTING */
+app.get("/", (req, res) => {
+  res.send("hello");
+});
